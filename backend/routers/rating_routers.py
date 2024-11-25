bef9cc78fed3647a3ca173c1543921daa09457d3
olsotss <145278536+olsotss@users.noreply.github.com>
2024-11-25 21:55:36 +0300

    Update rating_routers.py

--- backend/routers/rating_routers.py
+++ backend/routers/rating_routers.py
@@ -1,95 +1,53 @@
-from fastapi import APIRouter, Depends, HTTPException, Query
-from typing import List
+from typing import Dict, List, Union
+from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from database.database import get_film_db
-from schemas.description_schema import DescriptionRead
-from schemas.director_schema import DirectorRead
-from schemas.film_schema import FilmRead
-from schemas.genre_schema import GenreRead
-from services.description_service import DescriptionService
-from services.director_service import DirectorService
-from services.film_service import FilmService
-from services.genre_service import GenreService
+from repositories.film_repository import FilmRepository
+from repositories.rating_average_repository import RatingAverageRepository
+from repositories.rating_repository import RatingRepository
+from repositories.user_repository import UserRepository
+from schemas.rating_schema import FilmRating, MessageCreateRating, RatingCreate, RatingResponse
+from services.rating_average_service import RatingAverageService
+from services.rating_service import RatingService
 
-# rating_router = APIRouter()
+rating_router = APIRouter()
+@rating_router.post("/rating", response_model=MessageCreateRating)
+async def create_rating(rating: RatingCreate, db: AsyncSession = Depends(get_film_db)):
+    user_repository = UserRepository(db)
+    film_repository = FilmRepository(db)
+    rating_average_repository = RatingAverageRepository(db)
+    rating_repository = RatingRepository(db)
 
-# @rating_router.post("/", response_model=MessageCreateRating)
-# async def create_rating(rating: RatingCreate, db: AsyncSession = Depends(get_film_db)):
-#     user_sel = select(user_table.c.id).where(user_table.c.username == rating.username)
-#     user_result = await db.execute(user_sel)
-#     user = user_result.fetchone()
+    rating_average_service = RatingAverageService(rating_average_repository, film_repository)
 
-#     if not user:
-#         raise HTTPException(
-#             status_code=status.HTTP_404_NOT_FOUND,
-#             detail="User not found"
-#         )
+    rating_service = RatingService(user_repository, film_repository, rating_average_service, rating_repository)
 
-#     film_sel = select(film_table.c.id).where(film_table.c.title == rating.film_title)
-#     film_result = await db.execute(film_sel)
-#     film = film_result.fetchone()
-
-#     film_id = film[0]
-
-#     rating_sel = select(
-#         (rating_average_table.c.rating_average).label('average_rating'),
-#         (rating_average_table.c.rating_count).label('rating_count')
-#     ).where(rating_average_table.c.film_id == film_id)
-    
-#     rating_result = await db.execute(rating_sel)
-#     rating_data = rating_result.fetchone()
-
-#     current_avg_rating, current_rating_count = rating_data
-
-#     new_avg_rating = (current_avg_rating * current_rating_count + rating.rating_user) / (current_rating_count + 1)
-#     new_rating_count = current_rating_count + 1
-
-#     sel_update = update(rating_average_table).where(rating_average_table.c.film_id == film_id).values(
-#         rating_average=new_avg_rating,
-#         rating_count=new_rating_count
-#     )
-#     await db.execute(sel_update)
-
-#     stmt_insert = insert(rating_table).values(
-#         film_id=film_id,
-#         rating_user=rating.rating_user,
-#         review=rating.review,
-#         username=rating.username,
-#     )
-#     await db.execute(stmt_insert)
-#     await db.commit()
-
-#     return {"message": "Спасибо за отзыв!"}
+    message = await rating_service.create_rating(
+        username=rating.username,
+        film_title=rating.film_title,
+        rating_user=rating.rating_user,
+        review=rating.review,
+    )
+    return {"message": message}
 
 
-# @rating_router.get("/", response_model=Dict[str, Union[FilmRating, List[RatingResponse]]])
-# async def get_rating(film_title: str = Query(..., description="Название фильма"), db: AsyncSession = Depends(get_film_db)):
-#     film_sel = select(film_table.c.id).where(film_table.c.title == film_title)
-#     film_result = await db.execute(film_sel)
-#     film = film_result.fetchone()
+@rating_router.get("/rating", response_model=Dict[str, Union[FilmRating, List[RatingResponse]]])
+async def get_rating(film_title: str, db: AsyncSession = Depends(get_film_db)):
+    user_repository = UserRepository(db)
+    film_repository = FilmRepository(db)
+    rating_average_repository = RatingAverageRepository(db)
+    rating_repository = RatingRepository(db)
 
-#     film_id = film.id
+    rating_average_service = RatingAverageService(rating_average_repository, film_repository)
 
-#     rating_sel = select(rating_average_table).where(rating_average_table.c.film_id == film_id)
-#     rating_result = await db.execute(rating_sel)
-#     rating = rating_result.fetchone()
+    rating_service = RatingService(
+        user_repository=user_repository,
+        film_repository=film_repository,
+        rating_average_service=rating_average_service,
+        rating_repository=rating_repository
+    )
+    film_id = await film_repository.get_film_id_by_title(film_title)
+    film_rating = await rating_average_service.get_average_web_rating(film_title)
+    ratings_and_reviews = await rating_service.get_ratings_and_reviews(film_id)
 
-#     if not rating:
-#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Rating not found")
-
-#     sel = select(
-#         rating_table.c.rating_user,
-#         rating_table.c.review,
-#         rating_table.c.username,
-#         rating_average_table.c.rating_average,
-#         rating_average_table.c.rating_web 
-#     ).join(
-#         rating_average_table, rating_table.c.film_id == rating_average_table.c.film_id
-#     ).where(rating_table.c.film_id == film_id)
-#     result = await db.execute(sel)
-#     ratings = result.fetchall()
-
-#     return {
-#         "film_rating": FilmRating(rating_web=rating.rating_web, rating_average=rating.rating_average),
-#         "reviews": [RatingResponse(username=rating.username, rating_user=rating.rating_user, review=rating.review) for rating in ratings]
-#     }
+    return {"film_rating": film_rating, "reviews": ratings_and_reviews}
