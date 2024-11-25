02bfb53e228a913a3576adb535395a3306eef52e
olsotss <145278536+olsotss@users.noreply.github.com>
2024-11-25 16:43:27 +0300

    Update director_routers.py

--- backend/routers/director_routers.py
+++ backend/routers/director_routers.py
@@ -7,7 +7,7 @@ from services.director_service import DirectorService
 
 director_router = APIRouter()
 
-@director_router.get("/", response_model=List[DirectorRead])
+@director_router.get("/directors", response_model=List[DirectorRead])
 async def get_directors(genre_names: List[str] = Query(..., description="Список названий жанров"), db: AsyncSession = Depends(get_film_db)): 
     director_service = DirectorService(db)
     directors = await director_service.get_directors_by_genres(genre_names)
