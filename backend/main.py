05de42b0d2908eadbfa61cfd58a1654ed18defd0
olsotss <145278536+olsotss@users.noreply.github.com>
2024-11-25 13:57:09 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,6 @@ from routers.user_router import auth_router
 app = FastAPI()
 
 request_counter = defaultdict(list)
-
 limit = 5
 time_lim = 1
 
@@ -23,20 +22,16 @@ time_lim = 1
 async def limit_request(request: Request, call_next):
     user_ip = request.client.host 
     current_time = time()
-
     request_counter[user_ip] = [
         timestamp for timestamp in request_counter[user_ip]
         if timestamp > current_time - time_lim
     ]
-
     if len(request_counter[user_ip]) >= limit:
         return JSONResponse(
             content="Too Many Requests. Please try again later.",
             status_code=429
 )
-
     request_counter[user_ip].append(current_time)
-
     return await call_next(request)
 
 
