6f4d17952bde55cc119a328547050f8c46c9ad99
olsotss <145278536+olsotss@users.noreply.github.com>
2024-11-25 16:45:45 +0300

    Update genre_routers.py

--- backend/routers/genre_routers.py
+++ backend/routers/genre_routers.py
@@ -5,10 +5,9 @@ from database.database import get_film_db
 from schemas.genre_schema import GenreRead
 from services.genre_service import GenreService
 
-
 genre_router = APIRouter()
 
-@genre_router.get("/", response_model=List[GenreRead])
+@genre_router.get("/genres", response_model=List[GenreRead])
 async def get_genres(db: AsyncSession = Depends(get_film_db)):
     genre_service = GenreService(db)
     genres = await genre_service.get_all_genres()
