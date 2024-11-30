8dcf2599ec5d154efb81807a21c82e94f2ec3529
olsotss <145278536+olsotss@users.noreply.github.com>
2024-11-30 12:16:08 +0300

    Delete backend directory

--- backend/schemas/description_schema.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class DescriptionRead(BaseModel):
-    text: str
-
-    class Config:
-        orm_mode = True
--- backend/schemas/director_schema.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class DirectorRead(BaseModel):
-    name: str
-
-    class Config:
-        orm_mode = True
