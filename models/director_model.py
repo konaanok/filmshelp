dd681e1c7c56d6823407d88f89626790473280b9
olsotss <145278536+olsotss@users.noreply.github.com>
2024-11-30 10:45:04 +0300

    Delete models directory

--- models/director_model.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import MetaData, Table, Column, Integer, String
-metadata = MetaData()
-
-director_table = Table(
-    "director",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("name", String, nullable=False, primary_key=True)
-)
--- models/genre_model.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import MetaData, Table, Column, Integer, String
-metadata = MetaData()
-
-genre_table = Table(
-    "genre",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("name", String, nullable=False, primary_key=True)
-)
